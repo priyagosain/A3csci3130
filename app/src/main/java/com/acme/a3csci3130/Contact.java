ae79f139556df149b69527d01268353463851019
priyagosain <pr932087@dal.ca>
2017-07-12 13:37:02 -0300

    Business Contact

--- app/src/main/java/com/acme/a3csci3130/Contact.java
+++ app/src/main/java/com/acme/a3csci3130/Contact.java
@@ -15,25 +15,37 @@ import java.util.Map;
 public class Contact implements Serializable {
 
     public  String uid;
+    public  String business;
     public  String name;
-    public  String email;
+    public  String pbusiness;
+    public  String address;
+    public  String province;
+
+
 
     public Contact() {
         // Default constructor required for calls to DataSnapshot.getValue
     }
 
-    public Contact(String uid, String name, String email){
+    public Contact(String uid, String business, String name, String pbusiness, String address, String province){
         this.uid = uid;
+        this.business = business;
         this.name = name;
-        this.email = email;
+        this.pbusiness = pbusiness;
+        this.address = address;
+        this.province = province;
     }
 
     @Exclude
     public Map<String, Object> toMap(){
         HashMap<String, Object> result = new HashMap<>();
         result.put("uid", uid);
+        result.put("business", business);
         result.put("name", name);
-        result.put("email", email);
+        result.put("pbusiness", pbusiness);
+        result.put("address", address);
+        result.put("province", province);
+
 
         return result;
     }
--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -8,8 +8,9 @@ import android.widget.EditText;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
+    private EditText nameField, businessField, pbusinessField, addressField, provinceField;
     Contact receivedPersonInfo;
+    private MyApplicationData appState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +18,48 @@ public class DetailViewActivity extends Activity {
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
+        businessField  = (EditText) findViewById(R.id.business);
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        pbusinessField = (EditText) findViewById(R.id.pbusiness);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceField = (EditText) findViewById(R.id.province);
 
         if(receivedPersonInfo != null){
+            businessField.setText(receivedPersonInfo.business);
             nameField.setText(receivedPersonInfo.name);
-            emailField.setText(receivedPersonInfo.email);
+            pbusinessField.setText(receivedPersonInfo.pbusiness);
+            addressField.setText(receivedPersonInfo.address);
+            provinceField.setText(receivedPersonInfo.province);
+
+            //emailField.setText(receivedPersonInfo.email);
         }
     }
 
     public void updateContact(View v){
         //TODO: Update contact funcionality
+
+        String personID = receivedPersonInfo.uid;
+        String business = businessField.getText().toString();
+        String name = nameField.getText().toString();
+        String pbusiness = pbusinessField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getText().toString();
+
+        Contact contact = new Contact(personID, business, name, pbusiness, address, province);
+
+        // set values in firebase for the personID
+        appState.firebaseReference.child(receivedPersonInfo.uid).setValue(contact);
+        // return to main menu
+        finish();
+
     }
 
     public void eraseContact(View v)
     {
         //TODO: Erase contact functionality
+        // erase values in firebase for the personID
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        // return to main menu
+        finish();
     }
 }
